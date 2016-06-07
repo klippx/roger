552471f88ee866c2e585cfc2a95c9b14d507c95a
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-06-07 23:00:34 +0200

    Get webpack installed

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var Hello = React.createClass({
   render: function() {
     return React.createElement("h3", null, "Hello React!");
   }
 });
-
-console.log('Hello');
 
 ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   entry: './public/js/app.js',
+   output: {
+     path: './public',
+     filename: 'bundle.js',
+   },
+   module: {
+     loaders: [{
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+     }]
+   }
+ }
